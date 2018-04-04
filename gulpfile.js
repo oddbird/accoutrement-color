a3bcf60719c3e05fd982dc4aec096295766f45e9
Jonny Gerig Meyer <jonny@oddbird.net>
2018-04-04 12:18:26 -0400

    Fix bug/typo in _ac-color-get-function

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@
 
 'use strict';
 
+var beeper = require('beeper');
 var chalk = require('chalk');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
+var log = require('fancy-log');
 var sassdoc = require('sassdoc');
 var sasslint = require('gulp-sass-lint');
 
@@ -21,23 +22,21 @@ var paths = {
     ].concat(this.IGNORE);
     this.ALL_SASS = [
       this.SASS_DIR + '**/*.scss',
-      this.TEST_DIR + '**/*.scss',
-      // @@@ https://github.com/sasstools/sass-lint/issues/305
-      '!' + this.TEST_DIR + 'scss/_contrast.scss'
+      this.TEST_DIR + '**/*.scss'
     ].concat(this.IGNORE);
     return this;
   }
 }.init();
 
 var onError = function (err) {
-  gutil.log(chalk.red(err.message));
-  gutil.beep();
+  log.error(chalk.red(err.message));
+  beeper();
   this.emit('end');
 };
 
-var sasslintTask = function (src, failOnError, log) {
-  if (log) {
-    gutil.log('Running', '\'' + chalk.cyan('sasslint ' + src) + '\'...');
+var sasslintTask = function (src, failOnError, shouldLog) {
+  if (shouldLog) {
+    log('Running', '\'' + chalk.cyan('sasslint ' + src) + '\'...');
   }
   var stream = gulp.src(src)
     .pipe(sasslint())
@@ -62,16 +61,18 @@ gulp.task('sassdoc', function () {
     .pipe(sassdoc());
 });
 
-gulp.task('watch', function () {
-  gulp.watch(paths.SASS, ['sassdoc']);
+gulp.task('watch', function (cb) {
+  gulp.watch(paths.SASS, gulp.parallel('sassdoc'));
 
   // lint scss on changes
-  gulp.watch(paths.ALL_SASS, function (ev) {
-    if (ev.type === 'added' || ev.type === 'changed') {
-      sasslintTask(ev.path, false, true);
+  gulp.watch(paths.ALL_SASS).on('all', function (event, filepath) {
+    if (event === 'add' || event === 'change') {
+      sasslintTask(filepath, false, true);
     }
   });
 
   // lint all scss when rules change
-  gulp.watch('**/.sass-lint.yml', ['sasslint-nofail']);
+  gulp.watch('**/.sass-lint.yml', gulp.parallel('sasslint-nofail'));
+
+  cb();
 });
